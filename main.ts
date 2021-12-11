c5c6f4b55151b3b62791008d9042a2800a0183bd
valentijnpro <95985546+valentijnpro@users.noreply.github.com>
2021-12-11 23:05:24 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,16 @@
+let Piewer = game.createSprite(0, 0)
+let sprite = game.createSprite(2, 5)
+basic.forever(function () {
+    if (!(sprite.isTouching(Piewer))) {
+    	
+    }
+    if (!(false)) {
+    	
+    }
+    if (sprite.isTouching(Piewer)) {
+    	
+    }
+    if (true) {
+    	
+    }
+})
